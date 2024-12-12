ac6d6bbc98329292d39e1a8099c9312d15fed752
Gen0miX <j0naas_14@hotmail.com>
2024-12-12 23:13:10 +0100

    change package

--- start/env.ts
+++ start/env.ts
@@ -12,10 +12,10 @@
 import { Env } from '@adonisjs/core/env'
 
 export default await Env.create(new URL('../', import.meta.url), {
-  NODE_ENV: Env.schema.enum(['development', 'production', 'test'] as const),
-  PORT: Env.schema.number(),
+  NODE_ENV: Env.schema.enum.optional(['development', 'production', 'test'] as const),
+  PORT: Env.schema.number.optional(),
   APP_KEY: Env.schema.string(),
-  HOST: Env.schema.string({ format: 'host' }),
+  HOST: Env.schema.string.optional({ format: 'host' }),
   LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace']),
 
   /*
@@ -27,5 +27,5 @@ export default await Env.create(new URL('../', import.meta.url), {
   DB_PORT: Env.schema.number(),
   DB_USER: Env.schema.string(),
   DB_PASSWORD: Env.schema.string.optional(),
-  DB_DATABASE: Env.schema.string()
+  DB_DATABASE: Env.schema.string(),
 })
