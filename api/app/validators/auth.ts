a1a757044b795d8ed7b4d4e759b07e7efc1ba098
Gen0miX <j0naas_14@hotmail.com>
2025-01-07 15:31:36 +0100

    rollback to vine

--- api/app/validators/auth.ts
+++ api/app/validators/auth.ts
@@ -1,13 +1,21 @@
 import vine from '@vinejs/vine'
-import { schema, rules } from '@adonisjs/core/validator'
 
 const password = vine.string().minLength(8)
 
-export const registerValidator = schema.create({
-  email: schema.string({}, [rules.email(), rules.unique({ table: 'users', column: 'email' })]),
-  password: schema.string({}, [rules.confirmed()]),
-  username: schema.string({}, [rules.minLength(3), rules.maxLength(30)]),
-})
+export const registerValidator = vine.compile(
+  vine.object({
+    email: vine
+      .string()
+      .email()
+      .normalizeEmail()
+      .unique(async (db, value) => {
+        const match = await db.from('users').select('id').where('email', value).first()
+        return !match
+      }),
+    password,
+    username: vine.string().minLength(3),
+  })
+)
 
 export const loginValidator = vine.compile(
   vine.object({
