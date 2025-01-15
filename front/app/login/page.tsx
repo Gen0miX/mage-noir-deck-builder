9b7fc36f0d681498021e4a3315d0323a43097930
Gen0miX <J0naas_14@hotmail.com>
2025-01-15 10:19:42 +0100

    rm useSearchParams login page

--- front/app/login/page.tsx
+++ front/app/login/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 import { useEffect, Suspense } from "react";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useAuth } from "@/context/AuthContext";
 import LoginRegisterContainer from "@/components/LoginRegisterContainer";
 
 const Login: React.FC = () => {
   const { user } = useAuth();
   const router = useRouter();
-  const searchParams = useSearchParams();
-
-  const isVerified = searchParams.get("verified") === "true";
 
   useEffect(() => {
     if (user) {
